d7cdc17c2c494c00489b33f54d59cb22823c78db
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-11-11 00:08:57 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h2>회원가입 창임</h2>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
