068d671913da73b149a53ec7f8982eedd033886c
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-11-11 01:45:53 +0900

    회원가입 메인페이지 기초스타일링

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import "./App.css";
+import styled from "styled-components";
+import { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState("");
+
+  const currentTimer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const days = date.getDate();
+    setTime(`${year}.${month}.${days}`);
+  };
+
+  const startTimer = () => {
+    setInterval(currentTimer, 1000);
+  };
+
+  startTimer();
+
   return (
-    <div className="App">
-      <h2>회원가입 창임</h2>
+    <div className="wrap">
+      <div className="main">
+        <p className="time">{time}</p>
+        <h3 className="title">안녕하세요 원하는 기능을 확인 후 아래버튼을 눌러주세요</h3>
+        <div className="loginbox">
+          <button className="login" onClick={() => console.log("1")}>
+            로그인
+          </button>
+          <button className="join" onClick={() => console.log("2")}>
+            회원가입
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
